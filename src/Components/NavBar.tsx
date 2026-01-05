3479aadf271d662c05b0b51b9cb241268af583d0
tannerje <jtanner0786@gmail.com>
2026-01-04 22:13:29 -0500

    test change

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -9,7 +9,7 @@ export default function NavBar() {
     return (
         <header className="z-10 fixed flex justify-center pointer-events-none top-4 left-0 right-0">
             <nav className="pointer-events-auto overflow-hidden max-w-[95vw]">
-                <ul className="flex p-3 dark:bg-black border border-white/7 rounded-full gap-3 overflow-x-hidden w-full">
+                <ul className="flex p-3 dark:bg-black border border-white/7 rounded-full gap-3 overflow-x-auto scrollbar-hide">
                     <NavBoxes icon={IoHomeOutline} text="Home" href="/" />
                     <NavBoxes icon={IoIosInformationCircleOutline} text="About" href="/About" />
                     <NavBoxes icon={FaWrench} text="Tech" href="/#tech" />
